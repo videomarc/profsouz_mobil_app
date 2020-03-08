7969d0c11369f9cbfd0ffa8e411deb35c26c395d
videomarc <videoskaz@mail.ru>
2020-03-08 14:04:36 +0300

    profsouz_app

---
+++ profsouz_mobil_app/android/app/src/main/kotlin/com/example/profsouz_mobil_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.profsouz_mobil_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
